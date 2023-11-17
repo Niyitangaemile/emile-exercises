4853daa58d513c6041bbab85ad3dd7d0051100c8
Niyitangaemile <151012948+Niyitangaemile@users.noreply.github.com>
2023-11-17 09:35:34 +0200

    Add files via upload

---
+++ niyo/W2.java
@@ -0,0 +1,7 @@
+ class W2 {    
+public static void main(String[] args) {   
+    while(true){    
+        System.out.println("infinitive while loop");    
+    }    
+}    
+}     
---
+++ niyo/if2.java
@@ -0,0 +1,10 @@
+ class if2 {    
+public static void main(String[] args) {      
+    int age=20;  
+    int weight=80;     
+    if(age>=18){    
+        if(weight>50){  
+            System.out.println("You are eligible to donate blood");  
+        }    
+    }    
+}}   
